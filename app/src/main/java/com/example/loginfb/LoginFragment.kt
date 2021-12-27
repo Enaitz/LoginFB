9430e5001710da370620951d9857e0a95328fb47
Profenaitz <enaitz.echaguibel@institutvidreres.cat>
2021-12-27 10:37:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/loginfb/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.example.loginfb
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.loginfb.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_login, container, false
+        )
+        binding.setLifecycleOwner(this)
+
+
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+}
